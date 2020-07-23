07a3b38d434c26c1663ae0ea7fb6d76b7c3d1375
Lavi Sharma <lavisharma796@gmail.com>
2020-07-23 21:31:59 +0530

    create model, service and todos html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
@@ -11,12 +13,10 @@ import { HeaderComponent } from './layout/header/header.component';
     AppComponent,
     TodosComponent,
     TodoFormComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from '../model/Todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      { id: '222', title: 'Learn React', isCompleted: true, date: new Date() },
+      {
+        id: '111',
+        title: 'Learn Angular',
+        isCompleted: false,
+        date: new Date(),
+      },
+      {
+        id: '333',
+        title: 'Learn Svelte',
+        isCompleted: false,
+        date: new Date(),
+      },
+    ];
+  }
+
+  getTodos() {
+    return of(this.todos); // todos is now an obervable
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  changeStatus(todo: Todo) {
+    this.todos.map((singleTodo) => {
+      if (singleTodo.id === todo.id) {
+        singleTodo.isCompleted = !singleTodo.isCompleted;
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const indexOfTodo = this.todos.findIndex((t) => t.id === todo.id);
+    this.todos.splice(indexOfTodo, 1);
+  }
 }
